31367e98575edda522590f7c0e6f0336d184ca22
artemiikulikov <aapqwhat@gmail.com>
2023-12-06 12:22:58 +0300

    :star: 05 aoc not done

--- 05/main.go
+++ 05/main.go
@@ -4,6 +4,7 @@ import (
 	"aoc2023/common"
 	"fmt"
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -24,6 +25,20 @@ func transfer(inp int, m []mapped) int {
 		}
 	}
 	return inp
+}
+
+func mergeSlice(sl []seedRange) []seedRange {
+	sort.Slice(sl, func(i int, j int) bool { return sl[i].from < sl[j].from })
+	fmt.Println(sl)
+	new_sl := []seedRange{sl[0]}
+	for i := 1; i < len(sl); i++ {
+		if (sl[i].from + sl[i].count) < (new_sl[len(new_sl)-1].count + new_sl[len(new_sl)-1].from) {
+			new_sl[len(new_sl)-1].count = max(new_sl[len(new_sl)-1].count, sl[i].count)
+		} else {
+			new_sl = append(new_sl, sl[i])
+		}
+	}
+	return new_sl
 }
 
 func fillMapped(lines []string, m *[]mapped, i int) {
@@ -59,6 +74,29 @@ func fillSeeds(s []string) []int {
 			log.Fatal(err)
 		}
 		seeds = append(seeds, n)
+	}
+	return seeds
+}
+
+func fillSeeds2(s []string) []seedRange {
+	seeds := make([]seedRange, 0, len(s)-1)
+	for i := 1; i < len(s)-1; i++ {
+		var n int
+		n, err := strconv.Atoi(s[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		var n2 int
+		n2, err = strconv.Atoi(s[i+1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		sr := seedRange{
+			from:  n,
+			count: n2,
+		}
+		seeds = append(seeds, sr)
+		i++
 	}
 	return seeds
 }
@@ -103,7 +141,6 @@ func solve1(lines []string) {
 		seeds[i] = transfer(seeds[i], temperature_to_humidity)
 		seeds[i] = transfer(seeds[i], humidity_to_location)
 	}
-	fmt.Println(seeds)
 	minN := seeds[0]
 	for _, v := range seeds {
 		minN = min(v, minN)
@@ -111,9 +148,73 @@ func solve1(lines []string) {
 	fmt.Println(minN)
 }
 
+var memo = map[int]int{}
+
+type seedRange struct {
+	from  int
+	count int
+}
+
+func solve2(lines []string) {
+	var seeds []seedRange
+	seeds_to_soil := make([]mapped, 0)
+	soil_to_fertilizer := make([]mapped, 0)
+	fertilizer_to_water := make([]mapped, 0)
+	water_to_light := make([]mapped, 0)
+	light_to_temperature := make([]mapped, 0)
+	temperature_to_humidity := make([]mapped, 0)
+	humidity_to_location := make([]mapped, 0)
+	for i := range lines {
+		s := strings.Split(lines[i], " ")
+		switch s[0] {
+		case "seeds:":
+			seeds = fillSeeds2(s)
+		case "seed-to-soil":
+			fillMapped(lines, &seeds_to_soil, i+1)
+		case "soil-to-fertilizer":
+			fillMapped(lines, &soil_to_fertilizer, i+1)
+		case "fertilizer-to-water":
+			fillMapped(lines, &fertilizer_to_water, i+1)
+		case "water-to-light":
+			fillMapped(lines, &water_to_light, i+1)
+		case "light-to-temperature":
+			fillMapped(lines, &light_to_temperature, i+1)
+		case "temperature-to-humidity":
+			fillMapped(lines, &temperature_to_humidity, i+1)
+		case "humidity-to-location":
+			fillMapped(lines, &humidity_to_location, i+1)
+		}
+
+	}
+	// fmt.Println(seeds)
+	// seeds = mergeSlice(seeds)
+	fmt.Println(len(seeds))
+	minP := 9223372036854775806
+	for _, seed := range seeds {
+		var pn int
+		for j := seed.from; j < seed.from+seed.count; j++ {
+			// fmt.Println(j)
+			pn = transfer(j, seeds_to_soil)
+			pn = transfer(pn, soil_to_fertilizer)
+			pn = transfer(pn, fertilizer_to_water)
+			pn = transfer(pn, water_to_light)
+			pn = transfer(pn, light_to_temperature)
+			pn = transfer(pn, temperature_to_humidity)
+			pn = transfer(pn, humidity_to_location)
+			if minP > pn {
+				minP = pn
+				fmt.Println(minP)
+			}
+		}
+	}
+	fmt.Println(minP)
+}
+
 func main() {
 	lines := common.FileOpener()
 	test_lines := common.TestFileOpener()
-	solve1(test_lines)
-	solve1(lines)
+	//solve1(test_lines)
+	//solve1(lines)
+	solve2(test_lines)
+	solve2(lines)
 }
